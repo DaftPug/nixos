442ba83bbdb9c996ca22a0c7d114af2da08a7482
DaftPug <mm@pug-works.dk>
2024-06-12 07:02:02 +0200

    added firewall

--- configuration.nix
+++ configuration.nix
@@ -47,7 +47,10 @@
   # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
 
-  services.jellyfin.enable = true;
+  services.jellyfin = {
+    enable = true;
+    openFirewall = true;
+  };
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
